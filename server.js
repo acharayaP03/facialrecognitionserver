a6160f3885915a76bd4e63deec8aba426404033b
Prabhakar Acharya <acharyaP03@gmail.com>
2020-08-11 13:14:17 +1000

    fixing /profile/:id endpoint

--- server.js
+++ server.js
@@ -52,17 +52,19 @@ app.post('/register', (req, res) =>{
 app.get('/profile/:id', (req, res) =>{
 
     const { id } = req.params;
-    let found = false;
+    database.select('*').from('users').where({
+        id: id
+    })
+    .then( user =>{
 
-    db.users.forEach(user =>{
-        if(user.id === id){
-            found = true;
-            return res.json(user);
+        if(user.length){
+            res.json(user[0]);
+        }else{
+            res.status(400).json('Not found')
         }
-    });
-    if(!found){
-        res.status(400).json('not found')
-    }
+    })
+    .catch(err => res.status(400).json('error getting user'))
+
 })
 
 app.put('/image', (req, res) =>{
